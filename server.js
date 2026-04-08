223ca87de5917592c7c8c69cd0def96d781d8cde
Elin Ronda <elro2506@student.miun.se>
2026-04-08 20:20:19 +0200

    Försöker åtgärda serverproblem

--- server.js
+++ server.js
@@ -3,41 +3,50 @@
  */
 "use strict";
 
-console.log("JS funkar!");
+//Testar att servern går igång
+console.log("JavaScript funkar!");
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+//Port 3000 lokalt men Render använder process.env.PORT
 const port = process.env.PORT || 3000;
+//Importerar PostgreSQL
 const { Pool } = require("pg");
 
+//En pool för databasansltutningen
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    //Env-variabel från Render
+    connectionString: process.env.DATABASE_URL || "postgresql://cvuser:pvIAoDGNvKvTgcyjpc4TYGaQh1KoTTuF@dpg-d79ms6ea2pns73e7bkog-a.oregon-postgres.render.com/cv_iswp",
+    //Testar och ser om SSL hjälper till i Render för anslutningen
     ssl: {
         rejectUnauthorized: false
     }
 });
 
 
-
+//Testar att databasen funkar
 pool.query("SELECT NOW()", (err, res) => {
     if (err) {
-        console.log("DB error", err);
+        console.log("Databas-error", err);
     } else {
-        console.log("DB funkar");
+        console.log("Databasen funkar");
     }
 });
 
-
+//EJS som view enginge
 app.set("view engine", "ejs");
 app.use(express.static("public"));          //Statiska filer i katalogen Public
+//Här läser jag in data från formuläret
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-//Radera
+//Route för att radera kurs
 app.get("/courses/delete/:id", async (req, res) => {
   try {
+    //Kurs tas bort baserat på id
     await pool.query("DELETE FROM courses WHERE id = $1", [req.params.id]);
+    //Efter radering hamnar man på courses-sidan igen
     res.redirect("/courses");
   }    catch (err) {
     console.error(err);
@@ -45,11 +54,14 @@ app.get("/courses/delete/:id", async (req, res) => {
 });
 
 
-//Route
+//Routes
+
+//Startsidan
 app.get("/", (req, res) => {
     res.send("Du är live");
 });
 
+//Sidan för att lägga till kurs
 app.get("/courses/add", (req, res) => {
     res.render("addcourse", {
         errors: [],
@@ -60,9 +72,12 @@ app.get("/courses/add", (req, res) => {
     });
 });
 
+//Visar alla kurser
 app.get("/courses", async (req, res) => {
     try {
         const result = await pool.query("SELECT * FROM courses");
+
+        //Skickar all data till EJS-vyn
         res.render("courses", {
             courses: result.rows
         });
@@ -71,6 +86,7 @@ app.get("/courses", async (req, res) => {
     }
 });
 
+//Lägger till kurs med post
 app.post("/courses/add", async (req, res) => {
     const { coursecode, coursename, syllabus, progression } = req.body;
 
@@ -82,6 +98,7 @@ app.post("/courses/add", async (req, res) => {
     if (!syllabus) errors.push("Ange en kursplan");
     if (!progression) errors.push("Ange en progression");
 
+    //Felmeddelande vid fel
     if (errors.length > 0) {
         return res.render("addcourse", {
             errors,
@@ -94,10 +111,11 @@ app.post("/courses/add", async (req, res) => {
     }
 
     try {
+        //Lägger till ny kurs i databasen
         await pool.query(`INSERT INTO courses (coursecode, coursename, syllabus, progression) VALUES($1, $2, $3, $4)`,
             [coursecode, coursename, syllabus, progression]
         );
-
+//Man skickas till courses-sidan
         res.redirect("/courses");
     } catch (err) {
         console.error(err);
@@ -111,7 +129,7 @@ app.get("/about", (req, res) => {
 
 
 
-//Starta
+//Starta servern
 app.listen(port, () => {
     console.log("Server started on port: " + port);
 });
